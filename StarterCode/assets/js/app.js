ee4eda9a36549a5b67c4dcda1d5c476687c286fd
sandhiya <sandhyasoneta12@gmail.com>
2018-11-15 20:19:24 -0500

    found path

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,5 +1,5 @@
 // @TODO: YOUR CODE HERE!
-d3.csv("./data.csv", function(error, Data) {
+d3.csv("/assets/data/data.csv").then(function(Data, error) {
     if (error) return console.warn(error);
   
     console.log(Data);
